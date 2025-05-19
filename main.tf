3bd7cdd3330e82efe13a76f6eb4e4384c074b1f2
Jalil Ka <djalilk@gmail.com>
2025-05-19 07:49:24 -0400

    remove text to speech

--- main.tf
+++ main.tf
@@ -50,20 +50,3 @@ module "translate_function" {
   depends_on = [module.in-bucket, module.out-bucket]
   
 }
-
-module "text_to_speech_function" {
-  source = "./modules/cloud_function"
-  project_id = var.project_id
-  gcp_region = var.gcp_region
-  python_version = "python${var.python_version}"
-  function_name = local.speech_function_name
-  code_bucket_suffix = var.code_bucket_suffix
-  code_bucket_name = "${var.project_id}-${ var.code_bucket_suffix }"
-  cloud_function_archive = "${var.cloud_function_archive_prefix}-${ local.speech_function_name  }.zip"
-  source_bucket_name = module.out-bucket.bucket_name
-  target_bucket_name = module.audio-bucket.bucket_name
-  target_language = var.target_language
-  function_entry_point = "text_to_speech_converter"
-  depends_on = [module.in-bucket, module.out-bucket]
-  
-}
