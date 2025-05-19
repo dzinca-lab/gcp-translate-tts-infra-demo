b75feb4ac7790534c6413a463694036a22a6a2d5
Jalil Ka <djalilk@gmail.com>
2025-05-18 21:56:33 -0400

    Remove storage_class

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -12,8 +12,3 @@ output "bucket_location" {
   description = "The location of the storage bucket"
   value       = google_storage_bucket.bucket.location
 }
-
-output "bucket_storage_class" {
-  description = "The storage class of the bucket"
-  value       = google_storage_bucket.bucket.storage_class
-}
