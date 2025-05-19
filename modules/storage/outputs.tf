5af97d9c3d84bf797379bb071aeed09265a98c2c
Jalil Ka <djalilk@gmail.com>
2025-05-18 22:15:49 -0400

    Fix buckets

--- modules/storage/outputs.tf
+++ modules/storage/outputs.tf
@@ -1,14 +1,14 @@
 output "bucket_name" {
   description = "The name of the storage bucket"
-  value       = google_storage_bucket.bucket.name
+  value       = google_storage_bucket.tt_bucket.name
 }
 
 output "bucket_url" {
   description = "The URL of the storage bucket"
-  value       = google_storage_bucket.bucket.self_link
+  value       = google_storage_bucket.tt_bucket.self_link
 }
 
 output "bucket_location" {
   description = "The location of the storage bucket"
-  value       = google_storage_bucket.bucket.location
+  value       = google_storage_bucket.tt_bucket.location
 }
